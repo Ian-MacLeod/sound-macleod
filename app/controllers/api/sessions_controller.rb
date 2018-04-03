4c87572da8983f088abc7be75eec75d513fd5738
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-03 11:39:42 -0700

    Create User model with authentication

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      log_in(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid Login"], status: 403
+    end
+  end
+
+  def destroy
+    return render json ["You are already signed out"] unless logged_in?
+    log_out
+    render json: ["Successfully logged out"]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, default: :json do
+    resources :users, only: %i(create)
+    resource :session, only: %i(create destroy)
+  end
+  root to: "static_pages#root"
 end

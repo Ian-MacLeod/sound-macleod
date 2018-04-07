ab8bb0ad5a1e108e7d99a92afc381db55bb7ffc3
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-07 16:01:45 -0700

    Add play pause button to TrackIndexItem

--- frontend/components/main/tracks/track_index_item.jsx
+++ frontend/components/main/tracks/track_index_item.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { formatDuration } from "../../../utils/time_utils";
 import ImageDefault from "../../image_default";
+import PlayPauseButton from "../../play_pause_button/play_pause_button_container";
 
 const TrackIndexItem = ({
   track,
@@ -10,36 +11,43 @@ const TrackIndexItem = ({
   idx,
   showDelete,
   deleteTrack,
-  playTrack
-}) => (
-  <li onClick={() => playTrack(track.id)}>
-    <div className="left">
-      <div className="number">
-        <p>{idx}</p>
-        <div className="track-img">
-          <ImageDefault src={track.image.url} />
+  playTrack,
+  isPlaying
+}) => {
+  return (
+    <li
+      className={isPlaying ? "playing" : ""}
+      onClick={() => playTrack(track.id)}
+    >
+      <div className="left">
+        <div className="number">
+          <p>{idx}</p>
+          <div className="track-img">
+            <ImageDefault src={track.image.url} />
+            <PlayPauseButton />
+          </div>
+        </div>
+        <div className="info">
+          <p className="username" onClick={e => e.stopPropagation()}>
+            {user ? <Link to={`/users/${user.id}`}>{user.username}</Link> : ""}
+          </p>
+          <p className="track-title" onClick={e => e.stopPropagation()}>
+            <Link to={`/tracks/${track.id}`}>{track.title}</Link>
+          </p>
         </div>
       </div>
-      <div className="info">
-        <p className="username" onClick={e => e.stopPropagation()}>
-          {user ? <Link to={`/users/${user.id}`}>{user.username}</Link> : ""}
-        </p>
-        <p className="track-title" onClick={e => e.stopPropagation()}>
-          <Link to={`/tracks/${track.id}`}>{track.title}</Link>
-        </p>
+      <div className="length">
+        {showDelete ? (
+          <div onClick={e => e.stopPropagation()}>
+            <a onClick={deleteTrack} className="delete" />
+          </div>
+        ) : (
+          ""
+        )}
+        <p>{formatDuration(track.length)}</p>
       </div>
-    </div>
-    <div className="length">
-      {showDelete ? (
-        <div onClick={e => e.stopPropagation()}>
-          <a onClick={deleteTrack} className="delete" />
-        </div>
-      ) : (
-        ""
-      )}
-      <p>{formatDuration(track.length)}</p>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default TrackIndexItem;
--- frontend/components/main/tracks/track_index_item_container.js
+++ frontend/components/main/tracks/track_index_item_container.js
@@ -4,9 +4,13 @@ import { deleteTrack } from "../../../actions/track_actions";
 import { playTrack } from "../../../actions/player_actions";
 import TrackIndexItem from "./track_index_item";
 
-const mapStateToProps = (state, ownProps) => ({
-  user: state.entities.users[ownProps.track.userId]
-});
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.entities.users[ownProps.track.userId],
+    isPlaying:
+      state.ui.player.trackId === ownProps.track.id && state.ui.player.playing
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   deleteTrack: () => dispatch(deleteTrack(ownProps.track.id)),

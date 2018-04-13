5c0e0f70532a74a70b08ce989b8dbed8ab099c27
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-13 15:43:30 -0700

    Add working links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => (
   <footer>
     <div className="content">
-      <a href="">Github</a>
-      <a href="">LinkedIn</a>
+      <a href="https://github.com/Ian-MacLeod">Github</a>
+      <a href="https://www.linkedin.com/in/ian-macleod-6421a2137/">LinkedIn</a>
     </div>
   </footer>
 );

fdd307b91172949f29cbc38ee3567a90daadac14
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-15 09:46:35 -0700

    Make search query case insensitive

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::SearchController < ApplicationController
   def index
     # get playlists, users, tracks, use jbuilder for resp
-    query = params[:query]
+    query = params[:query].downcase
     @playlists = Playlist.where("lower(title) LIKE ?", "%#{query}%")
     @users = User.where("lower(username) LIKE ?", "%#{query}%")
     @tracks = Track.where("lower(title) LIKE ?", "%#{query}%")
